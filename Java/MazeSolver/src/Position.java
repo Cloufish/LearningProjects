928b5cb61bfdac60a2f834b912c268bf0968055c
Cloufisz <bartmok17@gmail.com>
2020-09-06 18:27:41 +0200

    Completed Maze project

---
+++ Java/MazeSolver/src/Position.java
@@ -0,0 +1,15 @@
+import java.util.LinkedList;
+
+public class Position{
+	public int x;
+	public int y;
+	
+	public Position(int y, int x) {
+		this.x = x;
+		this.y = y;
+	}
+	@Override
+	public String toString() {
+	    return "[" + x + ", " + y + "]";
+	}
+}
