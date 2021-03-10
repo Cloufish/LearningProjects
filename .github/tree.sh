37b85f736f3b13113ef164884cf96bf5b6cce093
Cloufish <bartmok17@gmail.com>
2021-03-10 13:55:15 +0100

    Updated

--- .github/tree.sh
+++ .github/tree.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash +x
 
 echo  "# Learning projects" > README.md
 echo -n "This repo is for project just for Learning purpose. I do not explicitly own any of the code that I've pushed here. It's to keep all code for ProjectBasedLearning found on https://github.com/tuvtran/project-based-learning.
