65cf4bb15b8837fdf69d074914a03f654e2a7cf2
Cloufish <bartmok17@gmail.com>
2021-03-25 15:09:18 +0100

    MEAN App Update

--- Programming_Languages/MEAN-Stack/src/app/app.module.ts
+++ Programming_Languages/MEAN-Stack/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
