30dd5c929111055d5cc9d1bdd9f7aace129bf3d8
Cloufish <cloufish@protonmail.com>
2021-06-10 19:04:50 +0200

    Updated

---
+++ Android_Apps/ToDoList/app/src/main/java/com/example/todoactivity/DialogCloseListener.java
@@ -0,0 +1,8 @@
+package com.example.todoactivity;
+
+import android.content.DialogInterface;
+
+public interface DialogCloseListener {
+
+    public void handleDialogClose(DialogInterface dialog);
+}
